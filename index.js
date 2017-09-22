002692259a698001cc3c399f163e2e1353f79f2a
SmeagolWorms4 <smeagolworms4@gmail.com>
2017-09-22 16:59:52 +0200

    Fix date

--- index.js
+++ index.js
@@ -24,10 +24,10 @@
         var date = new Date();
         var time = date.getFullYear() + '/' +
             ("0" + (date.getMonth() + 1)).slice(-2) + '/' +
-            ("0" + (date.getDate() + 1)).slice(-2) + ' ' +
-            ("0" + (date.getHours() + 1)).slice(-2) + ':' +
-            ("0" + (date.getMinutes() + 1)).slice(-2) + ':' +
-            ("0" + (date.getSeconds() + 1)).slice(-2);
+            ("0" + date.getDate()).slice(-2) + ' ' +
+            ("0" + date.getHours()).slice(-2) + ':' +
+            ("0" + date.getMinutes()).slice(-2) + ':' +
+            ("0" + date.getSeconds()).slice(-2);
         return '[' + time + '][' + mode + ']';
     };
     Object.defineProperty(console, 'debug', {
